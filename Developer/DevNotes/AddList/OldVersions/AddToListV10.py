29329af5fe44f2668e9cf54517b7e3149d894c10
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 15:43:36 -0700

    AddToListV10.py
    Version 10 of the add to list. Now past halfway done adding in the 21 versions of this file for the day.

---
+++ Developer/DevNotes/AddList/OldVersions/AddToListV10.py
@@ -0,0 +1,607 @@
+#AddList.py
+'''
+Notes file of items/mobs/blocks/achievements/features/etc to add into the game
+'''
+# July 2019
+# July 21st 2019
+''' List 7.21.2019
+Diamond # Added
+Coal # Added
+Charcoal # Added
+Iron bucket # Added
+Iron ingot # Added
+Iron nugget # Added
+Bronze ingot # Added
+Bronze nugget # Added
+Iron shield # added
+steel shield # added
+Black banner
+white banner
+red banner
+Green banner
+blue banner
+cyan banner
+ink sac # Added
+dead bush
+rose bush # Added
+Dandelion # Added
+Poppy # Added
+Oak wood axe # Added
+Oak wood hoe # Added
+Oak wood shovel # Added
+Oak wood pickaxe # Added
+Oak wood Sign
+book rtf
+book odt
+Leather boots # added
+Leather tunic # added
+Leather cap # added
+Leather pants # added
+Lime dye # Added
+Red sand # Added
+Yellow sand # Added
+Blue sand # Added
+Gravel # Added
+Sandstone # Added
+Creeper # Added
+Spider # Added
+Cave spider # Added
+Infested blocks
+Silverfish # Added
+Redstone # Added
+Redstone repeater # Added
+Hopper # Added
+Dropper # Added
+Minecart # Added
+Boat # Added
+Fox # Added
+Cat # Added
+Emerald # Added
+Tnt # Added
+Gunpowder # Added
+Flint # Added
+Fireworks
+Turtle egg
+Bamboo # Added
+Sugar cane # Added
+Vines # Added
+Ladder # Added
+Seaweed # Added
+Kelp # Added
+Daylight sensor # Added
+Redstone lamp # Added
+Lead # Added
+Nametag # Added
+Anvil # Added
+Chest # Added
+Loom # Added
+Gold trophy
+Bronze trophy
+Silver trophy
+Red bricks
+Shulker box (colors)
+Shulker
+Stuck shulker
+Slime block # Added
+Nether portal frame
+Dynamite # Added
+Gas tank
+Pipes
+Cars
+Trucks
+Vans
+Buses
+Air conditioner # Added
+Coffee (Java) # Added
+Pumpkin # Added
+Carved pumpkin # Added
+Jack-o-lantern # Added
+Chorus fruit # Added
+Chorus plant # Added
+Lectern # Added
+Bookshelf # Added
+Librarian villager # Added
+Cleric villager # Added
+Blacksmith villager # Added
+Hay bale # Added
+Barrels # Added
+Dispenser # Added
+Couch
+TV
+Table
+Armor stand
+Brewing stand
+More cauldrons
+Scaffolding blocks
+Bean bag
+Light saber (red, blue, green, yellow, orange, pink, cyan, white, black, light grey, dark grey, purple, magenta, brown)
+Soda
+Armchair
+Rope # Added
+Chain # Added
+Fire
+Slime # Added
+Slime ball
+Ghast tear # Added
+Diorite
+Andesite
+Granite
+Quartz # Added
+Cobweb # Added
+Spider eye # Added
+Glowstone dust # Added
+'''
+# July 22nd 2019
+''' List 7.22.2019
+tin ore # added
+tin ingot # added
+tin nugget # added
+tin armor # added
+tin bucket
+tin door
+aluminum ore # added
+aluminum ingot # added
+aluminum nugget # added
+aluminum armor # added
+aluminum bucket
+aluminum door
+''' 
+# July 22nd 2019 overnight
+''' List 7.22.2019 overnight
+Achievements in MCPyEdition
+As many as possible
+New mobs:
+Blue Python # Added
+Yellow Python # Added
+Cobra # Added
+Gecko # Added
+Chameleon # Added
+Zombie pigmen (if you don't have it) # Already added
+Firefox # Added
+Waterfox # Added
+Icefox # Added
+Frogs # Added
+Tadpoles # Added
+New blocks: (not sorted)
+Jack-in-the-box # Added
+Blueprints # Added
+Map # Added
+Item frame # Added
+Music discs 12x # Added
+String # added
+Tripwire hook # Added
+Cannon
+Oak wood chest
+Dark oak wood chest
+Golden chest
+Diamond chest
+Obsidian chest
+Iron chest
+Steel chest
+Cloud block # Added
+Storm cloud block # Added
+Campfire # Added
+Spit # Added
+Smoke machine # Added
+Oak wood barrel
+Dark oak wood barrel
+Steel barrel
+Iron barrel
+Tin barrel
+Aluminum barrel
+Silver barrel
+Gold barrel
+Cavestone barrel
+Diamond barrel
+Emerald barrel
+Doggy door
+Dog collar # Added
+Golden apple # Added
+Enchanted golden apple # Added
+Golden carrot # Added
+Chocolate block # Added
+Molten Chocolate  # Added
+Ice cube # Added
+Ice shard # Added
+Icicle # Added
+Blue torch
+Lantern
+Bug zapper
+Leather block
+Feather block
+Oak wood leaves
+Dark oak wood leaves
+Raspberry bush
+Blueberry bush
+Blackberry bush
+Achilles water
+Sickle # Added
+Hammer # Added
+Mallot # Added
+Nail
+Screw
+Plywood block
+Paint bucket
+Spray paint
+Empty aluminum can
+Canteen # Added
+Checkpoint 
+Charcoal block
+Expresso machine
+Vending machine
+Coffee maker
+Composter
+Tiki torch
+Lily pad # Added
+Cattails # Added
+Stripped logs # Added
+Stripped planks # Added
+Iron slab # Added
+Iron stairs # Added
+Iron fence # Added
+Iron wall # Added
+Iron fencegate # Added
+Steel slab # Added
+Steel stairs # Added
+Steel fence # Added
+Steel wall # Added
+Steel fencegate # Added
+Tin slab # Added
+Tin stairs # Added
+Tin fence # Added
+Tin wall # Added
+Tin fencegate # Added
+Aluminum slab # Added
+Aluminum stairs # Added
+Aluminum fence # Added
+Aluminum wall # Added
+Aluminum fencegate # Added
+Silver slab # Added
+Silver stairs # Added
+Silver fence # Added
+Silver wall # Added
+Silver fencegate # Added
+Bronze slab # Added
+Bronze stairs # Added
+Bronze fence # Added
+Bronze wall # Added
+Bronze fencegate # Added
+New items
+Cake # Added
+Bread
+Chocolate chip cookies # Added
+Cookies # Added
+Apple pie # Added
+Fishing rod
+Carrot on a stick # Added
+Wheat on a stick # Added
+Apple on a stick # Added
+Watermelon on a stick # Added
+Grappling hook # Added
+Parachute # Added
+Coffee bean # Added
+Cup of Java # Added
+'''
+# July 23rd 2019 overnight
+''' List 7.23.2019 overnight
+Knockback book up to 100 # Added
+Wither potion # Added
+Poison potion
+Healing potion # Added
+Lava sponge # Added
+Wet lava sponge # Added
+'''
+# July 24th 2019
+''' List 7.24.2019 overnight
+New blocks
+Chestnut # Added
+Coconut # Added
+Coconut milk
+Palm tree sapling # Added
+Chestnut tree sapling # Added
+Acorn # Added
+Acorn tree sapling # Added
+Sycamore tree sapling # Added
+Pine tree sapling # Added
+Pinecone # Added
+'''
+# July 25th 2019
+''' List 7.25.2019 overnight
+Areas:
+Overworld
+Nether
+The moon
+The end
+Tipbox
+Seaweed grows to the top of the water. Kelp usually only grows up to 4 blocks tall
+New items
+Banners
+Loom # Again, already added. I think you are getting closer to adding everything from MC1.14 in (second duplicate found)
+Cartography table # Added
+Fossils
+Turn bugs off
+Bug spray
+Shields
+Elytra
+Football helmet
+BlastProtectionI to BlastProtectionXXXVIII (38)
+Toilet # Added
+Sink # Added
+Bathtub
+Urinal
+Tiles
+Clocks
+portable iron clock
+Portable steel clock
+Radar block
+Portable radar
+Iron analog clock
+Iron digital clock
+steel analog clock
+Steel digital clock
+Bronze analog clock
+Bronze digital clock
+Silver analog clock
+Silver digital clock
+Obsidian analog clock
+Obsidian digital clock
+Diamond analog clock
+Diamond digital clock
+Lapis lazuli analog clock
+Lapis lazuli digital clock
+Emerald analog clock
+Emerald digital clock
+Gold analog clock
+Gold digital clock
+Aluminum analog clock
+Aluminum digital clock
+oak wood planks analog clock
+Oak wood planks digital clock
+Etc
+Grindstone
+Glowstone dust # Sean, this was already added (first duplicate found)
+Flower pot
+Bowl # Added
+Spoon # Added
+Fork # Added
+Spork # Added
+Knife # Added
+Katana # Addeed
+Squid # Added
+Baby squid # Added
+Blubber # Added
+Honey block (liquid) # Added
+maple tree
+Solid honey block
+Beehive block
+Sperm whale # Added
+Blue whale # Added
+trident
+Harpoon
+Iron oven
+Steel oven
+Iron stoveplate
+Steel stoveplate
+Tipbox about massive blubber drop from blue whale
+Tipbox about partial word search
+'''
+# July 26th 2019
+'''
+New items
+Pickles
+Vinegar
+Onion
+Spawn eggs of existing mobs
+Maple wood items
+Scute
+Arrows with effects
+Spikeballs variations (needed for morning star)
+Chiseled quartz block
+Lion
+Tiger
+Panther
+Leopard
+Quartz pillar
+Smooth squartz
+
+Pros of MC PYE
+▪Written in Python, so it is a lot lighter
+Ex:
+Java Hello World program 5-10 lines
+Python Hello World program 1-2 lines
+▪Open source
+▪It is like Minecraft but with every good mod installed and functional
+▪Not owned by Microsoft
+▪Thousands of features
+▪Works online/offline
+▪Game files are easy to modify and changable by all
+▪Thousands of features
+Now over 2000 blocks/mobs/items total
+More codenames
+• Minecraft Python Edition
+• Pycraft
+• Minecraft 2
+• Pymine
+• Python Super Sandbox
+'''
+# July 27th 2019
+'''
+Cardboard # Added
+Packed cardboard (doesn't melt) # Added
+Cardboard divider # Added
+Packed Cardboard divider # Added
+ETC
+'''
+# July 28th 2019
+'''
+
+'''
+'''
+Notes file 1 import 1
+August 13th 2019 work day
+
+From: August 13th 2019 (original)
+
+Vikings # Added
+Levitation potion
+Slow falling potion
+Nausea potion
+Wither potion
+Thorns enchantment # Added
+
+From: August 6th 2019 overnight notes
+
+Explosive dirt
+Packed snow
+Block disarmer
+Explosive coal ore
+Explosive iron ore
+Explosive gold ore
+Explosive steel ore
+Explosive obsidian 
+Explosive diamond ore
+Etc
+Bedrock buster
+Throwable wither skull
+Throwable fireball
+Lightning stick
+Frost walker enchantment
+
+From: July 26th 2019 overnight notes
+
+Pickles
+Vinegar
+Onion
+Spawn eggs of existing mobs
+Maple wood items
+Scute
+Arrows with effects
+Spikeballs variations (needed for morning star)
+Chiseled quartz block
+Lion
+Tiger
+Panther
+Leopard
+Quartz pillar
+Smooth squartz
+'''
+'''
+Notes file 1 import 2
+From: overnight notes August 13th 2019
+
+Minecraft PY edition
+Nether water# Added
+# evaporates instantly
+Explosive bed # Added
+Coral block imitators # Added
+# imitates a coral block on land, dies underwater
+coral blocks # Added
+Tube coral block# Added
+Brain coral block# Added
+Fire coral block# Added
+Bubble coral block# Added
+Horn coral block# Added
+Petrified wood# Added
+# only mineable with an iron pickaxe or better
+Slimy wood# Added
+# slippery like ice on land, infinite sliding when in water
+Icy wood# Added
+Oak wood monster egg # Added
+# Spawns termites
+End stone monster egg  # Added
+# Spawns endermite
+monster eggs of all blocks # Working on it A LOT to go
+spawn eggs of all mobs # Working on it (56/257 - August 14th 2019)
+'''
+''' 
+Overnight 8.18.2019
+
+Minecraft Python Edition ideas
+
+Global warming arctic
+Polar bears start losing fur and dying in this biome
+All ice and snow melt
+
+Egypt
+Giant 255x255x255 pyramid made of sandstone
+Nile river
+
+Mobs
+Mummies - spawn in pyramids, 10 health, 
+Easy: 0.5 damage per hit (0.25 hearts)
+Medium: 1 damage per hit (0.5 hearts)
+Hard: 2 damage per hit (1 heart)
+Drops:
+Paper, gold ingots
+Pharaoh - takes power from light sources to regenerate. 50 health, damage
+Easy: 4 damage (2 hearts) per hit
+Medium: 6 damage (3 hearts) per hit
+Hard: 8 damage (4 hearts) per hit
+Drops: golden power wand
+
+Blocks
+Clay slabs (all colors)
+Wool slabs (all colors)
+Concrete powder slabs (all colors)
+Concrete slaps (all colors)
+Double piston # ADDED
+Triple piston # ADDED
+Quadruple piston # ADDED
+Double sticky piston # ADDED
+Triple sticky piston # ADDED
+Quadruple sticky pistons # ADDED
+Ketchup # ADDED
+
+Items
+Chicken nugget - recovers 0.5 food # ADDED
+Diamond ingot # ADDED
+Arachnid agility: climb up walls, crawl through cobwebs # ADDED
+Raw Bacon # ADDED
+Cooked bacon # ADDED
+Beef patty # ADDED
+Pork patty # ADDED
+Chicken patty # ADDED
+Hamburger # ADDED
+Tomato # ADDED
+Ketchup # ADDED
+Bucket of ketchup # ADDED
+Bottle of ketchup # ADDED
+'''
+'''
+8.20.2019
+
+Upside-down daylight sensor # ADDED
+Wallmount daylight sensor # ADDED
+Seagull # ADDED
+Checkers biome
+Spawn charged creeper
+Spawn supercharged creeper
+Half slab
+quarter slab
+3 quarter slab
+Half block
+quarter block
+slab fence # A fence that can be placed on slabs, goes down diagonally
+Nether chest # Like an ender chest, but it teleports items around the Nether
+Command block
+Daylight sensor
+
+'''
+# Section for people who open the file
+''' | '''
+print ("You have opened a notes file for Minecraft Python Edition. This file is used to keep track of new items for development. Deleting this file will not cause harm to the game.")
+print ("There are only comments here. You cannot view them without viewing the file source. Please use an editor to open this file, and try again.")
+noMore = input("Press [ENTER] key to close")
+''' File info '''
+# Version: 8
+# Language: Python 3.7.1 (Compatible with Python 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1)(not yet tested with Python 3.8/3.9)
+# Size: 011,.044 bytes (011,.04 Kilobytes (base 10)) -base 2 calculation not available- (when this file reaches 100.4 kilobytes in size, a megabyte conversion will be added here) -base 2 calculation not available-
+# Lines: 00,.533
+# Unicode characters used: None
+# Written with: notepad++ (7.5.9 (64 bit))
+# Software group: FOSS (Free Open Source Software)
+# Last modified: August 14th 2019
+# Sharing status: share with anyone, don't claim as your own, don't sell for a profit
+''' End of file info '''
+''' | '''
+# End of file
