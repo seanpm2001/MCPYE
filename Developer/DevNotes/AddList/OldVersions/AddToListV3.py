d7617e1d8fbbe35e4529b1337276862c432d79ad
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 15:39:04 -0700

    AddToListV3.py
    Version 3 of the add list, a small update.

---
+++ Developer/DevNotes/AddList/OldVersions/AddToListV3.py
@@ -0,0 +1,292 @@
+#AddList.py
+''' List 7.21.2019
+Diamond # Added
+Coal # Added
+Charcoal # Added
+Iron bucket # Added
+Iron ingot # Added
+Iron nugget # Added
+Bronze ingot # Added
+Bronze nugget # Added
+Iron shield # added
+steel shield # added
+Black banner
+white banner
+red banner
+Green banner
+blue banner
+cyan banner
+ink sac # Added
+dead bush
+rose bush # Added
+Dandelion # Added
+Poppy # Added
+Oak wood axe # Added
+Oak wood hoe # Added
+Oak wood shovel # Added
+Oak wood pickaxe # Added
+Oak wood Sign
+book rtf
+book odt
+Leather boots # added
+Leather tunic # added
+Leather cap # added
+Leather pants # added
+Lime dye # Added
+Red sand # Added
+Yellow sand # Added
+Blue sand # Added
+Gravel # Added
+Sandstone # Added
+Creeper # Added
+Spider # Added
+Cave spider # Added
+Infested blocks
+Silverfish # Added
+Redstone # Added
+Redstone repeater # Added
+Hopper # Added
+Dropper # Added
+Minecart # Added
+Boat # Added
+Fox # Added
+Cat # Added
+Emerald # Added
+Tnt # Added
+Gunpowder # Added
+Flint # Added
+Fireworks
+Turtle egg
+Bamboo # Added
+Sugar cane # Added
+Vines # Added
+Ladder # Added
+Seaweed # Added
+Kelp # Added
+Daylight sensor # Added
+Redstone lamp # Added
+Lead # Added
+Nametag # Added
+Anvil # Added
+Chest # Added
+Loom # Added
+Gold trophy
+Bronze trophy
+Silver trophy
+Red bricks
+Shulker box (colors)
+Shulker
+Stuck shulker
+Slime block # Added
+Nether portal frame
+Dynamite # Added
+Gas tank
+Pipes
+Cars
+Trucks
+Vans
+Buses
+Air conditioner # Added
+Coffee (Java) # Added
+Pumpkin # Added
+Carved pumpkin # Added
+Jack-o-lantern # Added
+Chorus fruit # Added
+Chorus plant # Added
+Lectern # Added
+Bookshelf # Added
+Librarian villager # Added
+Cleric villager # Added
+Blacksmith villager # Added
+Hay bale # Added
+Barrels # Added
+Dispenser # Added
+Couch
+TV
+Table
+Armor stand
+Brewing stand
+More cauldrons
+Scaffolding blocks
+Bean bag
+Light saber (red, blue, green, yellow, orange, pink, cyan, white, black, light grey, dark grey, purple, magenta, brown)
+Soda
+Armchair
+Rope # Added
+Chain # Added
+Fire
+Slime # Added
+Slime ball
+Ghast tear # Added
+Diorite
+Andesite
+Granite
+Quartz # Added
+Cobweb # Added
+Spider eye # Added
+Glowstone dust # Added
+'''
+''' List 7.22.2019
+tin ore # added
+tin ingot # added
+tin nugget # added
+tin armor # added
+tin bucket
+tin door
+aluminum ore # added
+aluminum ingot # added
+aluminum nugget # added
+aluminum armor # added
+aluminum bucket
+aluminum door
+''' 
+''' List 7.22.2019 overnight
+Achievements in MCPyEdition
+
+As many as possible
+
+New mobs:
+Blue Python # Added
+Yellow Python # Added
+Cobra # Added
+Gecko # Added
+Chameleon # Added
+Zombie pigmen (if you don't have it) # Already added
+Firefox # Added
+Waterfox # Added
+Icefox # Added
+Frogs # Added
+Tadpoles # Added
+
+New blocks: (not sorted)
+
+Jack-in-the-box # Added
+Blueprints # Added
+Map # Added
+Item frame # Added
+Music discs 12x # Added
+String # added
+Tripwire hook # Added
+Cannon
+Oak wood chest
+Dark oak wood chest
+Golden chest
+Diamond chest
+Obsidian chest
+Iron chest
+Steel chest
+Cloud block # Added
+Storm cloud block # Added
+Campfire # Added
+Spit # Added
+Smoke machine # Added
+Oak wood barrel
+Dark oak wood barrel
+Steel barrel
+Iron barrel
+Tin barrel
+Aluminum barrel
+Silver barrel
+Gold barrel
+Cavestone barrel
+Diamond barrel
+Emerald barrel
+Doggy door
+Dog collar # Added
+Golden apple # Added
+Enchanted golden apple # Added
+Golden carrot # Added
+Chocolate block # Added
+Molten Chocolate  # Added
+Ice cube # Added
+Ice shard # Added
+Icicle # Added
+Blue torch
+Lantern
+Bug zapper
+Leather block
+Feather block
+Oak wood leaves
+Dark oak wood leaves
+Raspberry bush
+Blueberry bush
+Blackberry bush
+Achilles water
+Sickle 
+Hammer
+Mallot
+Nail
+Screw
+Plywood block
+Paint bucket
+Spray paint
+Empty aluminum can
+Canteen
+Checkpoint 
+Charcoal block
+Expresso machine
+Vending machine
+Coffee maker
+Composter
+Tiki torch
+Lily pad # Added
+Cattails # Added
+Stripped logs # Added
+Stripped planks # Added
+Iron slab # Added
+Iron stairs # Added
+Iron fence
+Iron wall
+Iron fencegate
+Steel slab # Added
+Steel stairs
+Steel fence
+Steel wall
+Steel fencegate
+Tin slab # Added
+Tin stairs
+Tin fence
+Tin wall
+Tin fencegate
+Aluminum slab # Added
+Aluminum stairs
+Aluminum fence
+Aluminum wall
+Aluminum fencegate
+Silver slab # Added
+Silver stairs
+Silver fence
+Silver wall
+Silver fencegate
+Bronze slab # Added
+Bronze stairs
+Bronze fence
+Bronze wall
+Bronze fencegate
+New items
+
+Cake # Added
+Bread
+Chocolate chip cookies # Added
+Cookies # Added
+Apple pie # Added
+Fishing rod
+Carrot on a stick # Added
+Wheat on a stick # Added
+Apple on a stick # Added
+Watermelon on a stick # Added
+Grappling hook # Added
+Parachute # Added
+Coffee bean # Added
+Cup of Java # Added
+'''
+
+''' List 7.23.2019 overnight
+Knockback book up to 100 # Added
+Wither potion # Added
+Poison potion
+Healing potion # Added
+Lava sponge # Added
+Wet lava sponge # Added
+'''
+# End of file
