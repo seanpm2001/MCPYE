585196c38a88848606fc9fc327a382002dbdd759
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 16:04:23 -0700

    TipBoxV1.py
    The very first version of the game tipbox. It has a decent amount of tips (32) but is still highly incomplete.

---
+++ NewWorld/TipBox/OldVersions/TipBoxV1.py
@@ -0,0 +1,115 @@
+# Start of script
+''' TIPS '''
+# Title
+print ("\tMinecraft Python Edition") # Title text
+print ("\t\t Tips\n\n") # Centered heading text
+# Randomization
+import random
+ranTip = int(32) # only adjust this value to change the range of tips. It does not work with 0, or negative numbers
+# Written tips section
+tip1 = str("Welcome to Minecraft Python Edition")
+tip2 = str("To be able to mine stone blocks, you need a pickaxe. Start with a wooden one")
+tip3 = str("To be safe, try sleeping at night.")
+tip4 = str("When mining, make sure to bring plenty of light sources, such as torches")
+tip5 = str("Sleeping in a bed sets a spawn point, so when you die, you come back to a certain point")
+tip6 = str("When you cut down a tree, items like saplings, apples, and sticks will fall. Make sure to collect everything before moving on")
+tip7 = str("You can instantly grow any plant with bonemeal. Kill skeletons to get some")
+tip8 = str("To swim faster in the water, start sprinting. You can also go within 1 block areas when swimming")
+tip9 = str("You can modify the game files to customize your experience. Just make sure you know what you are doing")
+tip10 = str("You can't mine stone or ores with an axe, hoe, or sword")
+tip11 = str("When holding a sword, you can't break any blocks.")
+tip12 = str("You can change settings to make it so when you die, you don't drop any items")
+tip13 = str("Want to see more of the game at a time? Change your render distance! This can also make the game go faster, or slower")
+tip14 = str("Ice is slippery, be careful")
+tip15 = str("When walking in soul sand, you will move a lot slower")
+tip16 = str("Water can't easily be placed in the nether")
+tip17 = str("You can use lava around soul sand to grow plants in the nether faster")
+tip18 = str("You don't have to kill a sheep to get wool. You can also use shears")
+tip19 = str("Use a silk touch enchantment book to get blocks that you can't normally get with normal tools")
+tip20 = str("Watch out for creepers! They will explode")
+tip21 = str("Don't look an enderman in the eye")
+tip22 = str("Iron golems spawn around villages. They defend villagers from hostile mobs.")
+tip23 = str("If you fall from 4 blocks or more onto hard blocks without any protection, you will take damage.")
+tip24 = str("With a feather falling enchantment book, you can take less fall damage from high distances")
+tip25 = str("Never dig straight down, unless you want to die")
+tip26 = str("When digging up, place a torch at your feet for any possible gravel or sand that may fall. The torch will break these. Also be prepared for falling water")
+tip27 = str("At the bottom of the world is bedrock. Breaking this layer will send you to the void. You can't build in the void, you will just die, even in creative mode")
+tip28 = str("TNT can be set off with a redstone torch, redstone, flint and steel, lava, fire charge, and more")
+tip29 = str("You can fall on beds to prevent some fall damage and also bounce")
+tip30 = str("Minecraft Python Edition can be played both offline and online")
+tip31 = str("To sneak, press shift. When sneaking, you can place certain blocks on other blocks, and also not fall off the edge of a high or dangerous place")
+tip32 = str("Minecraft Python Edition is written mostly in Python, except for some converters")
+''' Custom tips section '''
+tip10000 = str("") # Unknown range
+tip10001 = str("") # Unknown range
+tip10002 = str("") # Unknown range
+# Tip randomizer
+tipNum = int(random.randint(1, ranTip))
+# Tip printing (prints them to the screen)
+if (tipNum == 1):
+	print (str(tip1))
+if (tipNum == 2):
+	print (str(tip2))
+if (tipNum == 3):
+	print (str(tip3))
+if (tipNum == 4):
+	print (str(tip4))
+if (tipNum == 5):
+	print (str(tip5))
+if (tipNum == 6):
+	print (str(tip6))
+if (tipNum == 7):
+	print (str(tip7))
+if (tipNum == 8):
+	print (str(tip8))
+if (tipNum == 9):
+	print (str(tip9))
+if (tipNum == 10):
+	print (str(tip10))
+if (tipNum == 11):
+	print (str(tip11))
+if (tipNum == 12):
+	print (str(tip12))
+if (tipNum == 13):
+	print (str(tip13))
+if (tipNum == 14):
+	print (str(tip14))
+if (tipNum == 15):
+	print (str(tip15))
+if (tipNum == 16):
+	print (str(tip16))
+if (tipNum == 17):
+	print (str(tip17))
+if (tipNum == 18):
+	print (str(tip18))
+if (tipNum == 19):
+	print (str(tip19))
+if (tipNum == 20):
+	print (str(tip20))
+if (tipNum == 21):
+	print (str(tip21))
+if (tipNum == 22):
+	print (str(tip22))
+if (tipNum == 23):
+	print (str(tip23))
+if (tipNum == 24):
+	print (str(tip24))
+if (tipNum == 25):
+	print (str(tip25))
+if (tipNum == 26):
+	print (str(tip26))
+if (tipNum == 27):
+	print (str(tip27))
+if (tipNum == 28):
+	print (str(tip28))
+if (tipNum == 29):
+	print (str(tip29))
+if (tipNum == 30):
+	print (str(tip30))
+if (tipNum == 31):
+	print (str(tip31))
+if (tipNum == 32):
+	print (str(tip32))
+# end of program, enter to eiit
+noMore = input("Press [ENTER] key to quit")
+# End of script
