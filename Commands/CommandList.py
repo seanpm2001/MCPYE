d6b2cf9d8097b30ff1619c6d56f83e6675e6d9f2
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 16:00:55 -0700

    CommandList.py
    The current list of in-game commands. Highly incomplete.

---
+++ Commands/CommandList.py
@@ -0,0 +1,44 @@
+''' start of script '''
+# Command List
+operator1 = str("/") # Classic Minecraft compatibility
+operator2 = str(">>") # Python Edition compatibility
+''' divider '''
+# caller operators
+'''
+@ operators
+@p = nearest player
+@a = all players
+@e = all mobs
+@s = yourself
+@en = all entities
+@ne = nearest mobs
+@nen = nearest entities
+'''
+# Python mode
+>>say ""
+>>ban ""
+>>kick ""
+>>quit @s
+>>clear
+# Clears inventory
+>>effect
+# Applies a potion effect
+# Summon and spawn work the same. Both start differently. However, the summon command can only spawn 1 at a time
+>>summon
+>>spawn
+''' divider '''
+# exits the player from the world
+# Java/C++ mode
+/say ""
+/ban ""
+/kick ""
+/quit @s
+/clear @s
+# Clears inventory
+/effect @s
+# Applies a potion effect
+# Summon and spawn work the same. Both start differently. However, the summon command can only spawn 1 at a time
+/summon
+/spawn
+# exits the player from the world
+''' end of script '''
