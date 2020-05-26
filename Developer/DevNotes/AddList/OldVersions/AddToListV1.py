673875016e6d987998567c9d1a3d6bd965e758f1
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 15:37:40 -0700

    AddToListV1.py
    The very first version of the add to list, a note list. It already has a decent amount of notes added in.

---
+++ Developer/DevNotes/AddList/OldVersions/AddToListV1.py
@@ -0,0 +1,143 @@
+#AddList.py
+''' List 7.21.2019
+Diamond # Added
+Coal # Added
+Charcoal # Added
+Iron bucket # Added
+Iron ingot # Added
+Iron nugget # Added
+Bronze ingot # Added
+Bronze nugget # Added
+Iron shield # added
+steel shield # added
+Black banner
+white banner
+red banner
+Green banner
+blue banner
+cyan banner
+ink sac # Added
+dead bush
+rose bush
+Dandelion
+Poppy
+Oak wood axe
+Oak wood hoe
+Oak wood shovel
+Oak wood pickaxe
+Oak wood Sign
+book rtf
+book odt
+Leather boots # added
+Leather tunic # added
+Leather cap # added
+Leather pants # added
+Lime dye # Added
+Red sand # Added
+Yellow sand # Added
+Blue sand # Added
+Gravel # Added
+Sandstone # Added
+Creeper # Added
+Spider # Added
+Cave spider # Added
+Infested blocks
+Silverfish # Added
+Redstone # Added
+Redstone repeater # Added
+Hopper # Added
+Dropper # Added
+Minecart # Added
+Boat # Added
+Fox # Added
+Cat # Added
+Emerald # Added
+Tnt # Added
+Gunpowder # Added
+Flint # Added
+Fireworks
+Turtle egg
+Bamboo # Added
+Sugar cane # Added
+Vines # Added
+Ladder # Added
+Seaweed
+Kelp # Added
+Daylight sensor # Added
+Redstone lamp # Added
+Lead # Added
+Nametag # Added
+Anvil # Added
+Chest # Added
+Loom # Added
+Gold trophy
+Bronze trophy
+Silver trophy
+Red bricks
+Shulker box (colors)
+Shulker
+Stuck shulker
+Slime block # Added
+Nether portal frame
+Dynamite # Added
+Gas tank
+Pipes
+Cars
+Trucks
+Vans
+Buses
+Air conditioner # Added
+Coffee (Java) # Added
+Pumpkin
+Carved pumpkin
+Jack-o-lantern
+Chorus fruit
+Chorus plant
+Lectern # Added
+Bookshelf # Added
+Librarian villager
+Cleric villager
+Blacksmith villager
+Hay bale # Added
+Barrels # Added
+Dispenser # Added
+Couch
+TV
+Table
+Armor stand
+Brewing stand
+More cauldrons
+Scaffolding blocks
+Bean bag
+Light saber (red, blue, green, yellow, orange, pink, cyan, white, black, light grey, dark grey, purple, magenta, brown)
+Soda
+Armchair
+Rope # Added
+Chain # Added
+Fire
+Slime # Added
+Slime ball
+Ghast tear # Added
+Diorite
+Andesite
+Granite
+Quartz # Added
+Cobweb # Added
+Spider eye # Added
+Glowstone dust # Added
+'''
+''' List 7.22.2019
+tin ore # added
+tin ingot # added
+tin nugget # added
+tin armor # added
+tin bucket
+tin door
+aluminum ore # added
+aluminum ingot # added
+aluminum nugget # added
+aluminum armor # added
+aluminum bucket
+aluminum door
+'''
+# End of file
