2ef962abad330687b059834f5a79c4c9e9f33cac
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 15:57:55 -0700

    craftlist.py
    The latest version of the craft-able item list.

---
+++ Crafting/craftlist.py
@@ -0,0 +1,51 @@
+# Crafting recipes
+# Tools
+'''
+eye of ender
+
+1 ender pearl
+1 blaze powder
+'''
+'''
+oak wood sword
+1 oak wood plank
+1 oak wood plank
+1 stick
+'''
+'''
+Sunglasses
+
+S = stick
+P = stained glass pane
+0 = empty
+
+0 0 0
+
+P S P
+
+S 0 S
+
+Stained glass in any color works against the sun. This will reduce the blindness effect, but if you stare at it too long still, it will start to wear down the sunglasses.
+Looking into the sun also takes away night vision when you don't have eye protection
+
+'''
+# Structures
+'''
+Sun reactor core
+
+sun reactor core, middle, up 1
+beacon on top, sun reactor on bottom
+compact glass blocks around all open sun spots
+
+1 bronze nugget into beacon = 250 blocks of safe light
+1 bronze ingot into beacon = 500 blocks of safe light
+1 tin nugget into beacon = 750 blocks of safe light
+1 tin ingot into beacon = 1000 blocks of safe light
+1 iron nugget into beacon = 1500 blocks of safe light
+1 iron ingot into beacon = 2000 blocks of safe light
+1 redstone into beacon = 3000 blocks of safe light
+1 gold nugget into beacon = 4000 blocks of safe light
+1 gold ingot into beacon = 5000 blocks of safe light
+1 emerald into beacon = 7500 blocks of safe light
+1 diamond into beacon = 20000 blocks of safe light
+'''
