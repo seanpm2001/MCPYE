e3c09f379b7533885b9fbcc836d218bbd8b96b0e
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-25 23:21:37 -0700

    AchievementListV1.py
    First version of the achievement list file for MCPYE.

---
+++ Achievements/OldVersions/AchievementListV1.py
@@ -0,0 +1,30 @@
+''' start of script '''
+# Achievement list
+# View the achievements in the game, and their ID
+# There is going to be over 1000 achievements when ready
+''' divider ''' 
+# Start of list
+''' Overworld ''' # Category 1
+quest_firstDayID = float(0.1)
+quest_firstDayStr = str("First day\nSurvive the first night")
+quest_gettingDirtyID = float(0.2)
+quest_gettingDirtyStr = str("Getting dirty\nDig 256 dirt blocks without any tools")
+''' Nether ''' # Category 2
+quest_intoTheUnderworldID = float(1.1)
+quest_intoTheUnderworldStr = str("Into the underworld\nVisit the Nether for the first time")
+quest_eternalSadnessID = float(1.2)
+quest_eternalSadnessStr = str("Eternal sadness\nKill a ghast")
+''' Mining ''' # Category 3
+quest_coalMinerID = float(2.1)
+quest_coalMinerStr = str("Coal miner\nGather 3 coal while mining")
+''' End ''' # Category 4
+quest_dragonSlayerID = float(3.1)
+quest_dragonSlayerStr = str("Dragon slayer\nDefeat the ender dragon")
+''' Items ''' # Category 5
+quest_masterCraftsmanID = float(4.1)
+quest_masterCraftsmanStr = str("Master craftsman\nUse a crafting table to craft every item in the game")
+''' Exploring ''' # Category 6
+quest_adventureTimeID = float(5.1)
+quest_adventureTimeStr = str("Adventure time\nExplore every biome")
+# End of list
+''' end of script '''
