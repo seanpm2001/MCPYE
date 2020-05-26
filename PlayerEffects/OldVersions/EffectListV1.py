573be82e0c6df42ff32c4fa9062956afeb7bee75
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-25 23:13:12 -0700

    EffectListV1.py
    An archive of the oldest version of the effect list. Highly incomplete, and a long ways to go.

---
+++ PlayerEffects/OldVersions/EffectListV1.py
@@ -0,0 +1,58 @@
+# Effect list
+''' divider '''
+# Bad omen 1-40
+'''
+BadOmenI - Small raid, only 3 pillagers
+BadOmenII - Small raid, only 4 pillagers
+BadOmenIII - Small raid, only 5 pillagers
+BadOmenIV - Small raid, only 6 pillagers, 2 waves
+badOmenV - Small raid, only 7 pillagers, 2 waves
+badOmenVI - Small raid, only 8 pillagers, 2 waves
+badOmenVII - Small raid, only 9 pillagers, 2 waves
+badOmenVIII - Small raid, only 10 pillagers, 2 waves
+badOmenIX - Small raid, only 11 pillagers, 3 waves
+badOmenX - Small raid, only 12 pillagers, 3 waves
+badOmenXI - Small raid, only 13 pillagers, 3 waves
+badOmenXII - Small raid, only 14 pillagers, 3 waves
+badOmenXIII - Small raid, only 15 pillagers, 3 waves
+badOmenXIV - Small raid, only 16 pillagers, 4 waves
+badOmenXV - Average raid, only 17 pillagers, 4 waves
+badOmenXVI - Average raid, only 18 pillagers, 4 waves
+badOmenXVII - Average raid, only 19 pillagers, 4 waves
+badOmenXVIII - Average raid, only 20 pillagers, 4 waves
+badOmenXIX - Average raid, only 21 pillagers, 5 waves
+badOmenXX - Average raid, 22 pillagers, 5 waves
+badOmenXXI - Average raid, 23 pillagers, 5 waves
+badOmenXXII - Average raid, 24 pillagers, 5 waves
+badOmenXXIII - Average raid, 25 pillagers, 5 waves
+badOmenXXIV - Average raid, 26 pillagers, 5 waves
+badOmenXXV - Average raid, 27 pillagers, 5 waves
+badOmenXXVI - Average raid, 28 pillagers, 5 waves
+badOmenXXVII - Average raid, 29 pillagers, 5 waves
+badOmenXXVIII - Average raid, 30 pillagers, 5 waves
+badOmenXXIX - Average raid, 31 pillagers, 6 waves
+badOmenXXX - Average raid, 32 pillagers, 6 waves
+badOmenXXXI - Average raid, 33 pillagers, 6 waves
+badOmenXXXII - Average raid, 34 pillagers, 6 waves
+badOmenXXXIII - Average raid, 35 pillagers, 6 waves
+badOmenXXXIV - Strong raid, 36 pillagers, 6 waves
+badOmenXXXV - Strong raid, 37 pillagers, 6 waves
+badOmenXXXVI - Strong raid, 38 pillagers, 6 waves
+badOmenXXXVII - Strong raid, 39 pillagers, 6 waves
+badOmenXXXVIII - Strong raid, 40 pillagers, 6 waves
+badOmenXXXIX - Strong raid, 41 pillagers, 7 waves
+badOmenXL - Strong raid, 42 pillagers, 7 waves
+'''
+# Jump boost 1-10
+'''
+jumpBoostI - 1 slab higher jump
+jumpBoostII - 1 block higher jump
+jumpBoostIII - 1 block 1 slab higher jump
+jumpBoostIV - 2 block higher jump
+jumpBoostV - 2 block 1 slab higher jump
+jumpBoostVI - 3 block higher jump
+jumpBoostVII - 3 block 1 slab higher jump
+jumpBoostVIII - 4 block higher jump
+jumpBoostIX - 4 block 1 slab higher jump
+jumpBoostX - 5 block higher jump
+'''
