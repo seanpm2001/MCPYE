cdb8c35fa0cf07643c1803f0372e35ec2a192e74
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 16:10:00 -0700

    MCPYE_SettingsMainV1.py
    The very first version of the MCPYE Game settings file. It is not yet fully functional, and highly incomplete.

---
+++ GameSettings/OldVersions/MCPYE_SettingsMainV1.py
@@ -0,0 +1,43 @@
+# Start of script
+''' Minecraft Python Edition game settings '''
+change1 = str("no")
+print ("Minecraft Python Edition - Settings")
+input1 = input("Chat")
+print ("Censor swear words")
+print ("Show emojis")
+print ("Show Unicode characters")
+print ("Show commands")
+print ("Show chat")
+print ("Show player deaths")
+print ("Show player achievements")
+print ("Show colored text")
+input2 = input("Nether")
+print ("Pass through Nether portals")
+print ("Play Nether music")
+print ("Allow bed clicks")
+print ("Change brightness")
+input3 = input("End")
+print ("Play End music")
+print ("Allow bed clicks")
+print ("Change brightness")
+print ("Show end credits")
+input4 = input("Overworld")
+print ("Play overworld music")
+noMore = input("Press [ENTER] key to exit settings")
+print ("Exiting")
+# Currently incapable of changes, the game hasn't even entered a stable Alpha yet, as the game is still mainly a draft.
+if (change1 == "yes"):
+	print ("Saving changes")
+	print ("Quitting")
+else:
+	print ("No changes made, quitting")
+finalize1 = input("Changes kept, press [ENTER] to exit this file")
+'''
+File info
+File type: *.py (Python file)
+File size: 01,.337 Bytes (01,.337 Kilobytes)
+File version: 1 (October 5th 2019)
+Latest version: 1 (October 5th 2019)
+First version: 1 (October 5th 2019)
+'''
+# End of script
