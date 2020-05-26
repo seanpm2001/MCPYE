e08dc8332084f8e40274e887a7eb94ec38dac21c
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 15:55:50 -0700

    dimensioninfo.py
    The current list of info for dimensions. 4 available dimensions:
    * Overworld
    * Nether
    * end
    * moon

---
+++ NewWorld/DimensionInfo/dimensioninfo.py
@@ -0,0 +1,26 @@
+# Dimension list
+
+'''
+Overworld
+Main spawn for new worlds
+height: IN FILE
+'''
+'''
+Nether
+Through Nether Portal
+height: IN FILE
+if you fall through the void in the nether, you can't go any deeper. It will go down infinitely, and crash when it has an overflow (this could take several millennia
+'''
+'''
+End
+Through end portal
+height: IN FILE
+If you fall through the void in the end, you will fall into the overworld
+'''
+'''
+Moon
+Through rocket trip
+height: IN FILE
+The moon that circles your Minecraft world.
+Falling off the moon just brings you back to your world. It is not recommended, as you will land in a really far spot of your world, depending on the time of the Minecraft day
+'''
