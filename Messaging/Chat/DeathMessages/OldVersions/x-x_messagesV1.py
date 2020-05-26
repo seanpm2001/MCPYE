09b4dca4e870749704483793a353c87fdf445392
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-25 23:10:15 -0700

    x-x_messagesV1.py
    A copy of version 1 of the death messages. Highly incomplete.

---
+++ Messaging/Chat/DeathMessages/OldVersions/x-x_messagesV1.py
@@ -0,0 +1,42 @@
+# Start of script
+playerMainUsername = str("Sbeve")
+playerSecondUsername = str("Aexl")
+mobDanger1 = str("Zombie")
+entityBom1 = str("{[TNT]}")
+# Death messages
+print (str(playerMainUsername) + " stabbed himself")
+print (str(playerMainUsername) + " fell off a tall object")
+print (str(playerMainUsername) + " hit the ground too hard")
+print (str(playerMainUsername) + " was slain by " + str(mobDanger1))
+print (str(playerMainUsername) + " fell into lava while trying to escape " + str(mobDanger1))
+print (str(playerMainUsername) + " didn't live on the same world as " + str(mobDanger1))
+print (str(playerMainUsername) + " died of food poisoning")
+print (str(playerMainUsername) + " died of hunger")
+print (str(playerMainUsername) + " was blown up by " + str(entityBom1))
+print (str(playerMainUsername) + " withered away")
+print (str(playerMainUsername) + " didn't survive the snap")
+print (str(playerMainUsername) + " burned to death")
+print (str(playerMainUsername) + " tried to sleep in t̷h̷e̴ ̶v̵o̵i̵d̸")
+print (str(playerMainUsername) + " blew up in an explosive bed")
+print (str(playerMainUsername) + " drowned in a lake")
+print (str(playerMainUsername) + " drowned in an ocean")
+print (str(playerMainUsername) + " fell from a high distance, and landed on ice")
+print (str(playerMainUsername) + " missed the jump and died")
+print (str(playerMainUsername) + " fell out of the world")
+print (str(playerMainUsername) + " was fed to the sun")
+print (str(playerMainUsername) + " took an arrow to the knee one too many times")
+print (str(playerMainUsername) + " was shot to death")
+print (str(playerMainUsername) + " shot themself")
+print (str(playerMainUsername) + " died of thirst")
+print (str(playerMainUsername) + " was blinded by the sun")
+print (str(playerMainUsername) + " was crushed to death")
+print (str(playerMainUsername) + " was stabbed by " + str(playerSecondUsername))
+print (str(playerMainUsername) + " was shot by " + str(playerSecondUsername))
+noMore = input("Press [ENTER] to exit this program")
+# End of script
+'''
+File version: 1 September 16th 2019
+Lines: 42
+Language: Python 3.7.1
+Size: 02,.173 bytes
+'''
