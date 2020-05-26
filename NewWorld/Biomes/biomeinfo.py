25e6fae1af4caf97231a2c69981c93beef579230
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 15:51:17 -0700

    biomeinfo.py
    The latest version of the biome list for MCPYE.

---
+++ NewWorld/Biomes/biomeinfo.py
@@ -0,0 +1,82 @@
+# Biome list
+''' Areas '''
+# Overworld
+# Nether
+# End
+''' Overworld '''
+""" Superflat """
+biome0ID = int(0) # Superflat empty: A superflat biome with 0 blocks. Use a command, or set world preferences to contain a starting chest to get an area to built with. Not available in hardcore, survival, or adventure mode.
+biome1ID = int(1) # Superflat grass 1 deep: A superflat biome that is just grass for 1 block deep. Digging any of the blocks on the ground leads to the void. This world is not recommended for survival unless you are given items
+biome2ID = int(2) # Superflat ice 1 deep: A superflat biome that is just ice for 1 block deep. Digging any of the blocks on the ground leads to the void. This world is highly not recommended for survival unless you are given items
+biome3ID = int(3) # Superflat bedrock 1 deep: A superflat biome that is just bedrock for 1 block deep. Digging any of the blocks on the ground leads to the void. This world is good for creating structure. This world is heavily not recommended for survival unless you are given items
+biome4ID = int(4) # Superflat magma 1 deep: A superflat biome that is just 1 block deep of magma blocks. Digging any of the blocks on the ground leads to the void. This world is good for creating specific structures, and is heavily not recommended for survival mode unless you are given items
+biome5ID = int(5) # Superflat Magma 2 deep, with 1 block deep lava puddles
+biome6ID = int(6) # Superflat grass 2 deep: A superflat biome with 1 block of grass, and 1 block of dirt below it. A variation exists that replaces dirt with bedrock. This world is good for making structures, but not good for caverning undergrond. This world is not recommended for survival mode unless you have items
+biome7ID = int(7) # Superflat ice 2 deep: A superflat biome with 2 blocks of ice as the ground. Variations exist with 1 block of ice on top and 1 block of bedrock below. This world is good for making structures, but not good for caverning underground. Ice can be replaced with snow blocks, packed ice, blue ice, packed snow, snow, and green ice in world settings. This world is not recommended for survival mode unless you have items.
+biome8ID = int(8) # Superflat bedrock 2 deep: A superflat biome with 2 blocks of bedrock as the ground. This world can be used for creating structures. No variations exist. This world is not recommended for survival mode unless you have items.
+biome9ID = int(9) # Superflat grass 3 deep: A superflat biome with 1 block of grass on top, and 2 blocks of dirt below it. Variations exist to have 1 block of grass on top, 1 block of dirt in the middle, 1 block of bedrock on the bottom, and 1 block of grass on top and 2 blocks of bedrock below. This world is good for making structures, but not good for making caverns, unless the top and bottom blocks are replaced with slabs. This world is not recommended for survival mode unless you have itemms.
+biome10ID = int(10) # Superflat ice 3 deep: A superflat biome with 3 blocks of ice. Variations exist with
+biome11ID = int(11) # Superflat bedrock 3 deep: A superflat biome with 3 blocks of bedrock. This biome is good for building, but not very good for caverning. This biome is not recommended for survival mode uness you have items
+biome12ID = int(12) # Superflat grass 4 deep: A superflat biome with 1 block of grass on top, and 3 blocks of dirt below. Variations exist to have between 1-3 blocks of bedrock below, and 1-3 blocks of dirt below. This biome is decent for basic tunneling. This biome is not recommended for survival mode unless you have items
+biome13ID = int(13) # Superflat ice 4 deep: A superflat biome with 4 blocks of ice on top. Variations exist to have ice replaced with snow, snow blocks, packed snow, blue ice, packed ice, and green ice. Bedrock can be between 1-3 blocks deep. This world is not recommended for survival mode
+biome14ID = int(14) # Superflat bedrock 4 deep: A superflat biome with 4 blocks of bedrock. This is good for making structures, and basic tunneling. This biome is not recommended for survival mode unless you have items.
+biome15ID = int(15) # Superflat grass 5 deep: A superflat biome with 1 block of grass on top, and 4 blocks of dirt below. Variations exist to have between 1-4 blocks of bedrock below, or 1-4 blocks of dirt below. This biome is not recommended for survival without items
+biome16ID = int(16) # Superflat ice 5 deep
+biome17ID = int(17) # Superflat bedrock 5 deep (perfect for an underground enderman room)
+biome18ID = int(18) # Superflat TNT 1 deep: TNT is just a decoration here. Unless you lit a block of TNT above it, the TNT is just going to fall through. This biome is not recommended for survival mode with items.
+biome19ID = int(19) # Superflat TNT 2 deep: This biome can crash a low-end computer easily. It is not recommended to ignite the TNT. Severe lag may occur. Variations exist to have a block of bedrock below, but this will still cause lag.
+biome20ID = int(20) # Superflat TNT 3 deep: This biome can crash a high-end computer easily. It is not recommended to ignite the TNT. Severe lag may occur. Variations exist to have 1-2 blocks of bedrock below, but this will still cause lag.
+biome21ID = int(21) # Superflat TNT 4 deep: This biome can crash a high-end computer easily. It is not recommended to ignite the TNT. Severe lag may occur. Variations exist to have 1-3 blocks of bedrock below, but this will still cause lag.
+biome22ID = int(22) # Superflat TNT 5 deep: This biome can crash a high-end computer easily. It is not recommended to ignite the TNT. Severe lag may occur. Variations exist to have 1-4  blocks of bedrock below, but this will still cause lag.(the game breaker)
+biome23ID = int(23) # Superflat obsidian 1 deep: This biome is just 1 blocks of obsidian as a base. This biome is recommended for creative mode. Survival mode is not recommended without items.
+biome24ID = int(24) # Superflat obsidian 2 deep: This biome is just 2 blocks of obsidian as a base. This biome is recommended for creative mode. Survival mode is not recommended without items. Variations exist to have 1 block to be replaced with bedrock
+biome25ID = int(25) # Superflat obsidian 3 deep: This biome is just 3 blocks of obsidian as a base. This biome is recommended for creative mode. Survival mode is not recommended without items. Variations exist to have 1-2 blocks to be replaced with bedrock
+biome26ID = int(26) # Superflat obsidian 4 deep: This biome is just 4 blocks of obsidian as a base. This biome is recommended for creative mode. Survival mode is not recommended without items. Variations exist to have 1-3 blocks to be replaced with bedrock
+biome27ID = int(27) # Superflat obsidian 5 deep: This biome is just 5 blocks of obsidian as a base. This biome is recommended for creative mode. Survival mode is not recommended without items. Variations exist to have 1-4 blocks to be replaced with bedrock
+""" Infinite """
+biomeA1ID = float(1.1) # Taiga Hills 1
+biomeA2ID = float(1.2) # Taiga Hills 2
+biomeA3ID = float(1.3) # Green hills 1
+biomeA4ID = float(1.4) # Green hills 2
+biomeA5ID = float(1.5) # Green hills 3
+biomeA6ID = float(1.6) # Jungle 1
+biomeA7ID = float(1.7) # Jungle 2
+biomeA8ID = float(1.8) # Volcanic region 1: This is a biome with obsidian volcanoes. The outer ground is made of cobblestone, and magma blocks. Volcanoes are between 25x25 blocks and 100x100 blocks in size, and between 30 and 200 blocks tall. Volcanoes contain caverns and slow rising lava. In the bottom are pistons that automatically close the volcano once it overflows. Lava craters are common around the biome. There is no water in this biome
+biomeA9ID = float(1.9) # Volcanic region 2: This is a biome with obsidian volcanoes. This variation has the same as the first, but instead of slow rising lava, there is fast rising lava. There are also hidden chests that contain fire resistance potions
+biomeA10ID = float(1.10) # Ultimate extreme hills: This is an even higher version of the extreme hills biome. Instead of stopping at 256 blocks, these hills keep going up to 50,000,000 blocks. Fast access systems are included, such as levitation MMMMMMMMMM (10,.000) and more. You can build the best high maps here.
+biomeA11ID = float(1.11) # Extreme hills: This is the classic extreme hills biome. Mountains here go up to 256 blocks high.
+biomeA12ID = float(1.12) # Skyblock 1: This is a skyblock map. You start on a grassy island with a tree, and a chest. In the chest are 2 buckets, one with lava, and one with water. You make cobblestone, and explore to other islands. Other islands include a gravel island, held up by bedrock, a sand island held up by bedrock, and a ore island held up by bedrock.
+""" Old """
+biomeB1ID = float(2.1) # [256x128x256]: Classic 1: The classic Minecraft world.
+''' Nether '''
+""" Infinite """
+biomeC1ID = float(3.1) # Wither island 1: This biome is in the nether. It contains a lot of soul sand, and starts between Y 6+ and Y 12+. Soul sand covers the bedrock, and there are lots of wither skeletons roaming the area through a spawner.
+""" Flat """
+biomeD1ID = float(4.1) # Netherrack flat 1: This biome is a nether biome which is a flat section of netherrack. There are zombie pigmen that spawn here.
+biomeD2ID = float(4.2) # Soul sand flat 1: This biome is a nether biome which is a flat section of soul sand. No mobs spawn here.
+biomeD3ID = float(4.3) # Ghast house 1: This biome is a specialized ghast area. There are bridges, and hiding spots mostly made out of ghast proof materials, such as obsidian. There are several ghast spawners, and there is even a secret petrified oak wood house with paintings.
+biomeD4ID = float(4.4) # Lava flat: This biome is just a flat lava region.
+""" Old """
+biomeE1ID = float(5.1) # Nether reactor core section: This is a section that is made with a nether reactor core
+''' End '''
+""" Infinite """
+biomeF1ID = float(6.1) # End stone hills 1: This is an end biome with hills of end stone. Hills are between 1-20 blocks high.
+""" Flat """
+biomeG1ID = float(7.1) # End stone flat section 1: This is an end biome which is a flat field of end stone.
+""" Old """
+biomeH1ID = float(8.1) # End classic 1: This is the map of the original end dimension. There is only 1 island. There is 1 ender dragon. There are no bars around the end crystals. You spawn in a 5 by 5 room inside the island with an obsidian platform.
+""" End of info """
+# file info
+# File version: V03
+# total lines: 00,.082
+# total characters: 086,.808 bits, 010,.851 bytes (010,.851 Kilobytes (base 10)) -base 2 conversion unavailable- 000,.000 Megabytes (base 10) -base 2 conversion unavailable-
+# Special characters used: none
+# Encoding: UTF-8
+'''
+'''
+'''
+'''
+# I keep accidentally typing "superfloat" instead of "superflat"
+'''
+'''
+# End of file
