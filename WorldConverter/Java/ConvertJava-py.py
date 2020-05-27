25527678cdfe31d8aec80a51cc677182f834b6e9
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 18:27:15 -0700

    ConvertJava-py.py
    This is an incomplete script that converts a Java Edition world into a format that MCPYE can read.

---
+++ WorldConverter/Java/ConvertJava-py.py
@@ -0,0 +1,23 @@
+# Start of script
+'''
+This script is designed to convert a Java Edition world to a MCPYE readable world
+The entire folder of the Minecraft world has to be imported
+'''
+def minecraftWorld_folder():
+	print("Unavailable")
+def minecraftWorld_folderUploadDialog():
+	worldUploadURL = str(input("Enter the location of the world: "))
+	# I don't know how to do this further
+def gameVersion():
+  print("Select the version of Java Edition that the world was using:")
+  print("Java 1.0 [ID: ?]")
+print("This script is not yet functional")
+print("Please upload a Java Minecraft world folder with all its data")
+print(minecraftWorld_folderUploadDialog())
+quitConfirm = str(input("Press [ENTER] key to quit"))
+# End of script
+'''
+File version: 1 (May 26th 2020: 18:27)
+First version: 1 (May 26th 2020: 18:27)
+'''
+""" Blank space for compiler below """
