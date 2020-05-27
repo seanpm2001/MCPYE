a4314501676caee378dce687553dbc030481e340
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-26 18:23:09 -0700

    ConvertBedrock-py.py version 1
    This file is currently under development. I don't have very many methods yet. Its purpose is to convert a bedrock Minecraft world to a format that MCPYE can read. It won't convert it to Python.

---
+++ WorldConverter/Bedrock/ConvertBedrock-Py.py
@@ -0,0 +1,20 @@
+# Start of script
+'''
+This script is designed to convert a bedrock world to a MCPYE readable world
+The entire folder of the Minecraft world ahs to be imported
+'''
+def minecraftWorld_folder():
+	print("Unavailable")
+def minecraftWorld_folderUploadDialog():
+	worldUploadURL = str(input("Enter the location of the world: "))
+	# I don't know how to do this further
+print("This script is not yet functional")
+print("Please upload a Bedrock Minecraft world folder with all its data")
+print(minecraftWorld_folderUploadDialog())
+quitConfirm = str(input("Press [ENTER] key to quit"))
+# End of script
+'''
+File version: 1 (May 26th 2020: 18:23)
+First version: 1 (May 26th 2020: 18:23)
+'''
+""" Blank space for compiler below """
