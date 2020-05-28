8abef85816a94ce873cc48156b3475a87ebfd8f7
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-27 17:32:13 -0700

    craftlistV3.py
    An archive of the latest version of the craftable items list. From May 27th 2020

---
+++ Crafting/OldVersions/craftlistV3.py
@@ -0,0 +1,179 @@
+# Start of script
+# Crafting recipes
+# Craftlist.py
+# Tools
+'''
+eye of ender
+
+1 ender pearl
+1 blaze powder
+
+X = Blank
+E = Ender Pearl
+B = Blaze powder
+
+E X X
+B X X
+X X X
+
+B X X
+E X X
+X X X
+
+B E X
+X X X
+X X X
+
+E B X
+X X X
+X X X
+
+Other combinations not listed
+
+'''
+# Divider
+'''
+oak wood sword
+1 oak wood plank
+1 oak wood plank
+1 oak wood stick
+
+X = blank
+O = Oak wood plank
+S = Oak wood Stick
+
+X O X
+X O X
+X S X
+
+O X X
+O X X
+S X X
+
+X X O
+X X O
+X X S
+
+3 combinations
+'''
+# Divider
+'''
+Sunglasses
+
+S = stick
+P = stained glass pane
+0 = empty
+
+0 0 0
+P S P
+S 0 S
+
+P S P
+S 0 S
+0 0 0
+
+2 combinations
+
+Stained glass in any color works against the sun. This will reduce the blindness effect, but if you stare at it too long still, it will start to wear down the sunglasses.
+Looking into the sun also takes away night vision when you don't have eye protection
+
+'''
+# Divider
+'''
+Oak Wood crafting table
+
+O = Oak wood planks
+X = blank
+
+X X X
+X O O
+X O O
+
+X X X
+O O X
+O O X
+
+O O X
+O O X
+X X X
+
+X O O
+X O O
+X X X
+
+4 combinations
+
+'''
+# Divider
+'''
+Dark oak Wood crafting table
+
+D = Dark oak wood planks
+X = blank
+
+X X X
+X D D
+X D D
+
+X X X
+D D X
+D D X
+
+D D X
+D D X
+X X X
+
+X D D
+X D D
+X X X
+
+4 combinations
+
+'''
+# Divider
+'''
+Cobblestone furnace
+
+C = cobblestone
+X = blank
+
+C C C
+C X C
+C C C
+
+1 combination
+
+'''
+# Structures
+'''
+Sun reactor core
+
+sun reactor core, middle, up 1
+beacon on top, sun reactor on bottom
+compact glass blocks around all open sun spots
+
+1 bronze nugget into beacon = 250 blocks of safe light
+1 bronze ingot into beacon = 500 blocks of safe light
+1 tin nugget into beacon = 750 blocks of safe light
+1 tin ingot into beacon = 1000 blocks of safe light
+1 iron nugget into beacon = 1500 blocks of safe light
+1 iron ingot into beacon = 2000 blocks of safe light
+1 redstone into beacon = 3000 blocks of safe light
+1 gold nugget into beacon = 4000 blocks of safe light
+1 gold ingot into beacon = 5000 blocks of safe light
+1 emerald into beacon = 7500 blocks of safe light
+1 diamond into beacon = 20000 blocks of safe light
+'''
+# Divider
+''' 
+File info
+File type: *.py (Python file)
+Programming language: Python
+File version (As of May 27th 2020: 5:31 pm): 3 (May 27th 2020: 5:31 pm)
+Purpose: crafting recipes within the game
+Build date: May 27th 2020:5:31 pm
+First version: 1 (September 16th 2019)
+Line count (As of May 27th 2020: 5:31 pm): 179
+Character count (As of May 27th 2020: 5:31 pm): xx,xxx
+'''
+# End of script
